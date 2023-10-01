2ff2b0946e8ae77cb7b14269e30f8d16620f2842
Jerry Tarus <jerrytarus@gmail.com>
2023-10-01 11:25:23 +0300

    First class functions completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (fares) {
+    return function (fare) {
+      return fare * fares;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, selectFunc) {
+    return selectFunc(drivers);
+  }
+  
